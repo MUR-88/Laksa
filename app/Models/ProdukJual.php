73ae14d5a21bf2afabee620db76fce1081018608
MURS-88 <fdedi81@gmail.com>
2022-11-19 23:14:19 +0700

    fix model

--- app/Models/ProdukJual.php
+++ app/Models/ProdukJual.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Kategori extends Model
+class ProdukJual extends Model
 {
     use HasFactory, SoftDeletes;
 
